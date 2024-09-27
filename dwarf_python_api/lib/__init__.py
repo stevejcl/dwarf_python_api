8c2fe74e93fabbad97303382f1828650ea8bac19
stevejcl <stevejcl@users.noreply.github.com>
2024-09-27 22:01:50 +0200

    Update __init__.py

--- dwarf_python_api/lib/__init__.py
+++ dwarf_python_api/lib/__init__.py
@@ -1,3 +1,3 @@
-from .dwarf_utils import  motor_action, perform_time, perform_timezone, read_camera_exposure, read_camera_gain, read_camera_IR, permform_update_camera_setting, perform_takePhoto
+from .dwarf_utils import  motor_action, perform_time, perform_timezone, read_camera_exposure, read_camera_gain, read_camera_IR, perform_update_camera_setting, perform_takePhoto
 
 __version__ = '1.2.0'
