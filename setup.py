0ae91c4c58e6e9e0465ddc1538fada3c8f37f199
stevejcl <stevejcl@users.noreply.github.com>
2024-12-21 17:53:20 +0100

    Add needed directory for dwarf_ble_connect

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='stevejcl',
     packages= find_packages(include=['dwarf_python_api','dwarf_ble_connect']),  # Include the main package directory
     package_dir={'dwarf_python_api': 'dwarf_python_api','dwarf_ble_connect': 'dwarf_ble_connect'},  # Specify the root of the package
-    package_data={ 'dwarf_ble_connect' : [ '*','dist_js/**'],'dwarf_python_api': ['lib/*', 'proto/*'],},  # Specify package data relative to the main package directory
+    package_data={ 'dwarf_ble_connect' : [ '*','dist_js/**', 'lib/*'],'dwarf_python_api': ['lib/*', 'proto/*'],},  # Specify package data relative to the main package directory
     include_package_data=True,
     install_requires=[]
 )
